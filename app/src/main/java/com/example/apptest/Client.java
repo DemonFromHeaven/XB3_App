56f05e23d69e7065a86a707fec341da1f6bd889b
thakralr <thakralr@mcmaster.ca>
2019-04-03 17:06:37 -0400

    reverted to original layout with custom general settings

--- app/src/main/java/com/example/apptest/Client.java
+++ app/src/main/java/com/example/apptest/Client.java
@@ -1,65 +1,45 @@
 package com.example.apptest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Client extends AppCompatActivity {
 
-    private static final String TAG = "Client";
-
-    private SectionsAdapter mSectionsAdapter;
-    private ViewPager mViewPager;
-
-    private void setupViewPager(ViewPager viewPager) {
-        SectionsAdapter adapter = new SectionsAdapter(getSupportFragmentManager());
-        adapter.addFrag(new HomeFragment(), "Home");
-        adapter.addFrag(new FilterFragment(), "Filter");
-        adapter.addFrag(new SettingsFragment(), "Settings");
-        viewPager.setAdapter(adapter);
+    private void launchSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    return true;
-//                case R.id.navigation_filter:
-//                    return true;
-//                case R.id.navigation_settings:
-//                    return true;
-//            }
-//            return false;
-//        }
-//    };
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_filter:
+                    return true;
+                case R.id.navigation_settings:
+                    launchSettings();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
 
-        mSectionsAdapter = new SectionsAdapter(getSupportFragmentManager());
-
-        mViewPager = findViewById(R.id.container);
-        setupViewPager(mViewPager);
-
-        TabLayout tabLayout = findViewById(R.id.tabs_below);
-        tabLayout.setupWithViewPager(mViewPager);
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_black_24dp);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_filter_black_24dp);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_black_24dp);
-
-//        BottomNavigationView navigation = findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
